a391d68d71266402a2449c6e1c5ad8f2b88bdd30
Ben Peoples <ben@trinculosattic.com>
2013-01-04 15:28:19 -0500

    As shipped version!

--- FNSigns/FNSigns.ino
+++ FNSigns/FNSigns.ino
@@ -5,10 +5,10 @@
 #define LETTERS 6
 
 // milliseconds between updates
-#define SPEED 50 
+#define SPEED 50
 
 // Minimum brightness
-#define DIM 127
+#define DIM 20
 
 // Maximum brightness
 #define BRIGHT 255
@@ -28,21 +28,25 @@ uint8_t i;
 // This lets us run accurate cycles and deal with rollover.  
 uint32_t timer;
 
-// These keep track of various portions of the cycle
+uint16_t levels[6];
+uint8_t directions[6];
 
-// offset is the distance between each letters in radians
-float offset = 2*PI/LETTERS;
+uint8_t offset = 50;
 
-// cur is the current position.  Only needed because we want to minimize floating point ops
-float cur = 0;
-
-// Step is the step size between each cycle, in radians
-float step_ = 2*PI/STEPS;
+uint8_t step_size = 12;
 
 void setup()
 {
   wdt_reset(); // Reset and hope for the best
   wdt_disable(); // Disable the WDT
+
+  for(i = 0; i < LETTERS; i++)
+  {
+     levels[i] = 255 - offset*i; 
+     levels[i] = constrain(levels[i],DIM,BRIGHT);
+  }
+  
+
 
   wdt_reset(); // Reset the WDT
   wdt_enable(WDTO_120MS); // Enable WDT at 120ms -- arduino will reset within 120ms of a lockup
@@ -61,30 +65,42 @@ void loop()
     {
       x = 0;
     }
-    cur = step_ * x; // calculate our current step position.  Could also just do cur += step_ but I like this form better:
+    //cur = step_ * x; // calculate our current step position.  Could also just do cur += step_ but I like this form better:
                      // this form lets me ignore rollover, although the sin() function should deal with the rollover just fine
                      // we may see a decrease in precision if we're running for a long time.
     for(i = 0; i < LETTERS; i++) // Cycle through our 6 or 4 letters
     {
-      analogWrite(patch[i],map(sin(cur+offset*i),-1,1,DIM,BRIGHT)); 
-/*
-    This requires a little more explanation.
-    
-    patch[0] returns 3, patch[1] returns 5.  This lets us get all 6 PWM pins in a sequence without having to do any real tricks.
-    offset*i returns the offset for the current letter.  That is, offset*0 is no offset, offset*1 is the first offset, etc.
-    
-    cur+offset*i therefore returns the current angular position of our chase for this letter
-    sin() returns -1 to 1
-    We use the map function to map from -1 to 1 to DIM to BRIGHT.   This lets us scale the brightness of the chase. 
-    In this case, I want to go from 127 to 255.
-    
-    The end result, is basically:
-    analogWrite(3,127--255); with the range 127 to 255 being determined by a sin wave function.
+      if(directions[i] == 0) // Go down
+      {
+         levels[i] -= step_size;
+         if(levels[i] <= DIM)
+         {
+            directions[i] = 1; // Go up! 
+         }
+  
+      }
+      else if(directions[i] == 1)
+      {  
+          if(levels[i] + step_size > levels[i])
+          {
+          levels[i] += step_size;
+          }
+          else
+          {
+             directions[i] = 0; // Going to overflow, go down.
+          }
+          
+          if(levels[i] >= BRIGHT)
+          {
+             directions[i] = 0; // Go down! 
+          }
+  
+      }      
+      analogWrite(patch[i],constrain(levels[i],0,255)); 
 
-
-*/
     }
   }
   wdt_reset();  
 }
+
 
